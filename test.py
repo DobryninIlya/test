f77b1f02da7c1cec52ce17366dbca85391582b6c
legalchik <93884665+legalchik@users.noreply.github.com>
2022-10-20 20:11:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def test():
-  return {"status": 'ok'}
-
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050)
+print("1923875982175251")
   
