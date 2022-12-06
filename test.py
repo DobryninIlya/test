d071a783df07205dd80eb51da73ae3ced3686268
legalchik <93884665+legalchik@users.noreply.github.com>
2022-12-06 20:40:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 print(0)
-  
+
+try:
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return "hello world"
+
+    if __name__ == "__main__":
+        app.run("0.0.0.0", port=80)
+except Exception as e:
+    print(e)
