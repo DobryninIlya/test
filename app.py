e71313ead66d19d6749186c002154fdf804d6f91
legalchik <93884665+legalchik@users.noreply.github.com>
2022-12-06 22:13:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-print(0)
+from flask import Flask
+from art import text2art
+from time import sleep
+import os
 
-try:
-    from flask import Flask
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def index():
-        return "hello world"
 
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run("0.0.0.0", port=port, debug=True)
-except Exception as e:
-    print(e)
+@app.route("/")
+def index():
+    return text_render("200")
+
+
+@app.route("/<text>")
+def text(text):
+    return text_render(text)
+
+
+def text_render(text: str):
+    yield "<pre>"
+    for el in text2art(text, font='block', chr_ignore=True).split("\n"):
+
+        for i in el:
+            yield i
+            sleep(0.005)
+        yield "<br>"
+
+    yield "</pre>"
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run("0.0.0.0", port=port, debug=True)
