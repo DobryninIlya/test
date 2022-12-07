cfd681571acaf8885f505b81de416004fd518c59
legalchik <93884665+legalchik@users.noreply.github.com>
2022-12-07 22:25:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,12 @@
-from flask import Flask
-from art import text2art
+from requests import get
 from time import sleep
-import os
 
 
-app = Flask(__name__)
+def main():
+    r = get("https://legalchik.onrender.com/ping")
+    print(r, r.text)
 
 
-@app.route("/ping"):
-def ping():
-    return "pong"
-
-
-@app.route("/")
-def index():
-    return text_render("200")
-
-
-@app.route("/baby")
-def baby():
-    return text_render("dilbarik")
-
-
-@app.route("/<text>")
-def text(text):
-    return text_render(text)
-
-
-def text_render(text: str):
-    yield "<pre>"
-    for el in text2art(text, font='block', chr_ignore=True).split("\n"):
-
-        for i in el:
-            yield i
-            sleep(0.005)
-        yield "<br>"
-
-    yield "</pre>"
-
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run("0.0.0.0", port=port, debug=True)
+while True:
+    main()
+    sleep(599)
