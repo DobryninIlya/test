df4549a90ede79515a5e45e7c852d0e3fab83f05
legalchik <93884665+legalchik@users.noreply.github.com>
2022-12-12 19:43:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
 async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    await bot.set_webhook(WEBHOOK_URL)
 
 
 async def on_shutdown(dispatcher):
@@ -39,7 +39,6 @@ if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
